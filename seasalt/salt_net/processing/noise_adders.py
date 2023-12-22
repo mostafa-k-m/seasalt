17e7ba60d9f51b271f14cbf4f32c7dfbfb673cc2
Mostafa Kamal <mos.kamal@nu.edu.eg>
2023-12-22 16:55:16 +0200

    fixed gaussian noise and added Random

--- seasalt/salt_net/processing/noise_adders.py
+++ seasalt/salt_net/processing/noise_adders.py
@@ -1,6 +1,7 @@
 from enum import Enum
-from typing import Callable, Tuple
+from typing import Callable, Dict, List, Tuple
 
+import numpy as np
 import torch
 
 
@@ -9,22 +10,31 @@ class NoiseType(Enum):
     SAP = 2
     BERNOULLI = 3
     POISSON = 4
+    RANDOM = 5
 
-    @property
-    def get_noise_func(self) -> Callable:
-        funcs = {
-            "GUASSIAN": _gaussian_noise_adder,
-            "SAP": _salt_and_pepper_noise_adder,
-            "BERNOULLI": _bernoulli_noise_adder,
-            "POISSON": _poisson_noise_adder,
+    def get_noise_func(
+        self,
+    ) -> Callable:
+        funcs: Dict[str, List[Callable]] = {
+            "GUASSIAN": [_gaussian_noise_adder],
+            "SAP": [_salt_and_pepper_noise_adder],
+            "BERNOULLI": [_bernoulli_noise_adder],
+            "POISSON": [_poisson_noise_adder],
+            "RANDOM": [
+                _gaussian_noise_adder,
+                _salt_and_pepper_noise_adder,
+                _bernoulli_noise_adder,
+                _poisson_noise_adder,
+            ],
         }
-        return funcs[self.name]
+        return np.random.choice(funcs[self.name])  # type: ignore
 
 
 def noise_adder(
     images: torch.Tensor, noise_parameters: torch.Tensor, noise_type: NoiseType
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    noisy_images, masks = noise_type.get_noise_func(images, noise_parameters)
+    noise_func = noise_type.get_noise_func()
+    noisy_images, masks = noise_func(images, noise_parameters)
     return noisy_images, masks.float()
 
 
@@ -38,7 +48,17 @@ def _gaussian_noise_adder(
         ]
     )
     noisy_images = (images + noise).clip(0, 1)
-    return noisy_images, noise != 0
+    return (
+        noisy_images,
+        torch.logical_and(
+            torch.logical_or(noisy_images == 0, noisy_images == 1),
+            ~torch.logical_or(images == 0, images == 1),
+        )
+        # torch.logical_or(
+        #     noise > noise[noise >= 0].quantile(0.1),
+        #     noise < noise[noise < 0].quantile(0.1),
+        # ),
+    )
 
 
 def _salt_and_pepper_noise_adder(
@@ -57,7 +77,7 @@ def _bernoulli_noise_adder(
     a = noise_parameters * torch.ones(images.shape)
     noise = torch.bernoulli(a)
     noisy_images = images * noise
-    return noisy_images, noise != 0
+    return noisy_images, noise == 0
 
 
 def _poisson_noise_adder(
@@ -67,4 +87,4 @@ def _poisson_noise_adder(
     p = torch.poisson(a)
     noise = p / p.max()
     noisy_images = (images + noise).clip(0, 1)
-    return noisy_images, noise != 0
+    return noisy_images, noise > 0
