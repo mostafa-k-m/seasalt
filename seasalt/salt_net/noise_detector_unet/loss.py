44b5d887373dd34865bf88e5caef84649294aa1e
Mostafa Kamal <mos.kamal@nu.edu.eg>
2023-12-22 16:55:41 +0200

    reimplemented dice loss

--- seasalt/salt_net/noise_detector_unet/loss.py
+++ seasalt/salt_net/noise_detector_unet/loss.py
@@ -1,20 +1,8 @@
 from typing import Optional
 
 import torch
+import torch.nn.functional as F
 from torch.nn import BCELoss
-
-
-def dice_loss(
-    input: torch.Tensor,
-    target: torch.Tensor,
-    epsilon: float = 1e-6,
-) -> torch.Tensor:
-    inter = 2 * (input * target).sum(dim=(-1, -2, -3))
-    sets_sum = input.sum(dim=(-1, -2, -3)) + target.sum(dim=(-1, -2, -3))
-    sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
-
-    dice = (inter + epsilon) / (sets_sum + epsilon)
-    return 1 - dice.mean()
 
 
 class DiceLoss(BCELoss):
@@ -27,5 +15,21 @@ class DiceLoss(BCELoss):
     ) -> None:
         super().__init__(weight, size_average, reduce, reduction)
 
+    def dice_loss(
+        self,
+        input: torch.Tensor,
+        target: torch.Tensor,
+        epsilon: float = 1e-6,
+    ) -> torch.Tensor:
+        inter = 2 * (input * target).sum(dim=(-1, -2, -3))
+        sets_sum = input.sum(dim=(-1, -2, -3)) + target.sum(dim=(-1, -2, -3))
+        sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
+
+        dice = (inter + epsilon) / (sets_sum + epsilon)
+        return 1 - dice
+
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        return BCELoss.forward(self, input, target) + dice_loss(input, target)
+        return (
+            F.binary_cross_entropy(input, target) + self.dice_loss(input, target)
+        ).mean()
+        # return BCELoss.forward(self, input, target) + dice_loss(input, target)
