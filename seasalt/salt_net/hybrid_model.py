dcb2e61f0b4630582e05f4ea1c4c0e3437d176dc
Mostafa Kamal <mostafa.k.mahmoud@zoho.com>
2025-02-24 02:48:32 +0200

    fixed some hardcoded parameters

--- seasalt/salt_net/hybrid_model.py
+++ seasalt/salt_net/hybrid_model.py
@@ -12,14 +12,21 @@ else:
 
 
 class HybridModel(torch.nn.Module):
+
     def __init__(
         self,
-        denoiser_weights_path,
+        denoiser_weights_path=None,
         enable_seconv=True,
         enable_fft=False,
         enable_anisotropic=True,
         enable_unet_post_processing=True,
+        auto_encoder_depth=5,
+        seconv_depth=7,
+        fft_depth=7,
+        anisotropic_depth=5,
         transformer_depth=10,
+        filters=48,
+        chnl_expansion_factor=2.66,
     ):
         super(HybridModel, self).__init__()
         denoiser_model = DenoiseNet(
@@ -27,7 +34,13 @@ class HybridModel(torch.nn.Module):
             enable_fft=enable_fft,
             enable_anisotropic=enable_anisotropic,
             enable_unet_post_processing=enable_unet_post_processing,
-            transformer_depth=10,
+            auto_encoder_depth=auto_encoder_depth,
+            seconv_depth=seconv_depth,
+            fft_depth=fft_depth,
+            anisotropic_depth=anisotropic_depth,
+            transformer_depth=transformer_depth,
+            filters=filters,
+            chnl_expansion_factor=chnl_expansion_factor,
         )
         if denoiser_weights_path:
             denoiser_model.load_state_dict(
